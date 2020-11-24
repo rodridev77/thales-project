ec4979da3f54eb9b85cc4cd43c7aef4e1be098ef
Maou Yami <maou@yamitec.com>
2020-11-24 15:39:02 -0300

    add js method to search address info from VIACEP api on Employee Edit and Create views

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -183,6 +183,7 @@ $title = "Atualização de Funcionario"
         </div>
     </div>
 </form>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script>
     $("#frmavatar").on("change", function() {
         var input = document.getElementById("frmavatar");
@@ -191,6 +192,65 @@ $title = "Atualização de Funcionario"
         fReader.onloadend = function(event) {
             var img = document.getElementById("avatar");
             img.src = event.target.result;
+        }
+    });
+
+    // get Address info from cep api
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        $("input[name='stree']").val("");
+        $("input[name='district']").val("");
+        $("input[name='city']").val("");
+        $("input[name='uf']").val("");
+    }
+
+    //Quando o campo cep perde o foco.
+    $("input[name='zipcode']").blur(function() {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = $(this).val().replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if (validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                $("input[name='stree']").val("...");
+                $("input[name='district']").val("...");
+                $("input[name='city']").val("...");
+                $("input[name='uf']").val("...");
+
+                //Consulta o webservice viacep.com.br/
+                $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function(dados) {
+
+                    if (!("erro" in dados)) {
+                        //Atualiza os campos com os valores da consulta.
+                        $("input[name='street']").val(dados.logradouro);
+                        $("input[name='district']").val(dados.bairro);
+                        $("input[name='city']").val(dados.localidade);
+                        $("input[name='uf']").val(dados.uf);
+                    } //end if.
+                    else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                        alert("CEP não encontrado.");
+                    }
+                });
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
         }
     });
 </script>
