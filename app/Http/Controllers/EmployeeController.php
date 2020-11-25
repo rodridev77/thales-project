79ea1addcd0eaa4b4dce98bb0e93ee9206886dc5
Maou Yami <maou@yamitec.com>
2020-11-25 14:47:34 -0300

    refactor method store on employee

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -72,4 +72,57 @@ class EmployeeController extends ControllersExtends
 
         return parent::update($request, $id);
     }
+
+    public function store(Request $request){
+        $imagePath = null;
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $name = time().'.'.$image->getClientOriginalExtension();
+            $destinationPath = public_path('/images');
+            $image->move($destinationPath, $name);
+            $imagePath = "/images\/".$name;
+        }
+
+        $employee = [
+            "image" => $imagePath,
+            "name"=> $request->name,
+            "birthday"=>$request->birthday,
+            "mother_name" => $request->mother_name,
+            "father_name" => $request->father_name,
+            "cpf" => $request->cpf,
+            "rg" => $request->rg,
+            "phone"=> $request->phone,
+            "gender" => $request->gender,
+            "email" => $request->email,
+            "level_of_schooling" => $request->level_of_schooling
+        ];
+        $address = [
+            "uf" => $request->uf,
+            "city" => $request->city,
+            "district" => $request->district,
+            "street" => $request->street,
+            "zipcode" => $request->zipcode,
+            "number" => $request->number,
+        ];
+        $bank = [
+            "account_number" => $request->account_number,
+            "bank"=>$request->bank,
+            "agency" => $request->agency,
+        ];
+
+        $contract = [
+            "cargo" => $request->cargo,
+            "salary" => $request->salary,
+            "admission_date" => $request->admission_date,
+            "dismission_date" => $request->dismission_date,
+        ];
+        parent::withAndChange([
+            Employee::class => $employee,
+            Address::class => $address,
+            BankData::class => $bank,
+            Contract::class => $contract],
+        ["permiss" => true, "key" => "employee_id"]);
+
+        return parent::store($request);
+    }
 }
