35a774b3b39630ad9fbc8682455c54895152e26f
Maou Yami <maou@yamitec.com>
2020-11-25 12:58:54 -0300

    update Shop modules, add sendrequest on wellcome, and fix errors on Extensions e Implements Controllers

---
+++ resources/views/shops/create.blade.php
@@ -0,0 +1,44 @@
+@extends('content_container_card')
+@php
+$title = "Cadastro de Loja"
+@endphp
+@section('card-body')
+<form data-saveemployee="{{url('/lojas/add')}}" method="POST">
+    @csrf
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">Informacoes basicas</h3>
+                    <div class="card-tools">
+                        <button class="btn btn-success" type="submit"> <i class="fa fa-plus"></i> Cadastrar</button>
+                    </div>
+                </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+
+                <div class="card-body">
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="exampleInputEmail1">Nome Fantasia</label>
+                            <input type="text" name="fantasyname" class="form-control" id="exampleInputEmail1" placeholder="Nome Fantasia">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="exampleInputEmail1">Razao Social</label>
+                            <input type="text" name="companyname" class="form-control" id="exampleInputEmail1" placeholder="Razao Social">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="exampleInputtext1">CPF</label>
+                            <input type="text" name="cnpj" class="form-control" id="exampleInputtext1" placeholder="CNPJ">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="exampleInputtext1">RG</label>
+                            <input type="text" name='ie' class="form-control" id="exampleInputtext1" placeholder="IE">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+@endsection
