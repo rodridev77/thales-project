9828492ac56455fa20d4206bc211b8326758fad0
Maou Yami <maou@yamitec.com>
2020-11-26 13:05:56 -0300

    fix bugs on employees views and controllers

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -25,6 +25,14 @@ $route = route("funcionarios.index");
                             <img width="65" height="65" id="avatar" />
                             <input type="file" name="image" class="form-control" id="frmavatar" placeholder="avatar" style="height: 65px; margin-top: -65px; opacity: 0">
                         </div>
+                        <div class="form-group col-md-8">
+                            <label for="exampleInputEmail1">Loja</label>
+                            <select class="form-control" id="exampleInputtext1" name="shop_id">
+                                @foreach ($shops as $shop)
+                                    <option value="{{ $shop->id }}">{{ $shop->fantasyname }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-8">
@@ -186,6 +194,8 @@ $route = route("funcionarios.index");
 </form>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script>
+    $(document).getAddress();
+
     $("#frmavatar").on("change", function() {
         var input = document.getElementById("frmavatar");
         var fReader = new FileReader();
@@ -193,65 +203,6 @@ $route = route("funcionarios.index");
         fReader.onloadend = function(event) {
             var img = document.getElementById("avatar");
             img.src = event.target.result;
-        }
-    });
-
-    // get Address info from cep api
-    function limpa_formulário_cep() {
-        // Limpa valores do formulário de cep.
-        $("input[name='stree']").val("");
-        $("input[name='district']").val("");
-        $("input[name='city']").val("");
-        $("input[name='uf']").val("");
-    }
-
-    //Quando o campo cep perde o foco.
-    $("input[name='zipcode']").blur(function() {
-
-        //Nova variável "cep" somente com dígitos.
-        var cep = $(this).val().replace(/\D/g, '');
-
-        //Verifica se campo cep possui valor informado.
-        if (cep != "") {
-
-            //Expressão regular para validar o CEP.
-            var validacep = /^[0-9]{8}$/;
-
-            //Valida o formato do CEP.
-            if (validacep.test(cep)) {
-
-                //Preenche os campos com "..." enquanto consulta webservice.
-                $("input[name='stree']").val("...");
-                $("input[name='district']").val("...");
-                $("input[name='city']").val("...");
-                $("input[name='uf']").val("...");
-
-                //Consulta o webservice viacep.com.br/
-                $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function(dados) {
-
-                    if (!("erro" in dados)) {
-                        //Atualiza os campos com os valores da consulta.
-                        $("input[name='street']").val(dados.logradouro);
-                        $("input[name='district']").val(dados.bairro);
-                        $("input[name='city']").val(dados.localidade);
-                        $("input[name='uf']").val(dados.uf);
-                    } //end if.
-                    else {
-                        //CEP pesquisado não foi encontrado.
-                        limpa_formulário_cep();
-                        alert("CEP não encontrado.");
-                    }
-                });
-            } //end if.
-            else {
-                //cep é inválido.
-                limpa_formulário_cep();
-                alert("Formato de CEP inválido.");
-            }
-        } //end if.
-        else {
-            //cep sem valor, limpa formulário.
-            limpa_formulário_cep();
         }
     });
 </script>
