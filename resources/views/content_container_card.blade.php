946c2c1f1ea53cb1d6f598e4f230628a24dfbd14
Maou Yami <maou@yamitec.com>
2020-11-26 08:53:44 -0300

    add back link on content_container dinamically, but is necessary add  value on views

--- resources/views/content_container_card.blade.php
+++ resources/views/content_container_card.blade.php
@@ -2,7 +2,7 @@
     <div class="card">
         <div class="card-header">
             <h3 class="card-title">
-                <a href="#" onclick="loadViewInHome('{{$route}}')">
+                <a href="#" onclick="loadViewInHome('{{$route ?? "home"}}')">
                     <i class='fas fa-arrow-left' style='font-size:16px;color: rgba(0, 123, 255, .7);'></i><span style="margin-left:5px">voltar</span>
                 </a>
                 {{$title ?? ""}}
