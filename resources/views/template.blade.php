e52d50b41d7202769ccf5e7c2a24b33e31c0ace7
Maou Yami <maou@yamitec.com>
2020-11-26 20:40:16 -0300

    remove AdminLTE Header Name, and update username to logged user name

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -67,7 +67,7 @@
             <a href="../../index3.html" class="brand-link">
                 <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+                <span class="brand-text font-weight-light">Loja 1.0</span>
             </a>
 
             <!-- Sidebar -->
@@ -78,7 +78,7 @@
                         <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
+                        <a href="#" class="d-block">{{ Auth::user()->name }} </a>
                     </div>
                 </div>
 
