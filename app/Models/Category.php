bcbd0c4dc127041caf546b69d0c396f3af279828
Marcos Paulo <markus.rodrigues7@gmail.com>
2020-12-03 17:14:08 -0200

    fix bug Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,7 +10,7 @@ class Category extends Model
     use HasFactory;
 
     public $timestamps = false;
-    protected $fillable = ["name","subacategory"];
+    protected $fillable = ["name","subcategory"];
 
     public function products()
     {
