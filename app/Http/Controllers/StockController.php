2102e0ad939929eacbf2d47c34e3d0955dd2284c
Maou Yami <maou@yamitec.com>
2020-12-04 15:36:08 -0300

    create a masks and update modules

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -1,10 +1,16 @@
 <?php
 
-namespace App\Http\Controllers;
+use App\Extensions\ControllersExtends;
+use App\Models\Stock;
 
 use Illuminate\Http\Request;
 
-class StockController extends Controller
+class StockController extends ControllersExtends
 {
-    //
+    public function __construct($model = null, $template = null) {
+        parent::__construct(Stock::class, "stocks");
+        parent::setValidate([
+            "name" => "required|unique:brands"
+        ]);
+    }
 }
