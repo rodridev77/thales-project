23c1e81bc16438c2f9acc63dce52b4cd7e49ccda
Maou Yami <maou@yamitec.com>
2020-12-06 21:16:57 -0300

    send updates on template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Lojas West</title>
+    <link rel="shortcut icon" href="../../dist/img/lwlogo.gif" />
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
@@ -62,8 +63,8 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="../../index3.html" class="brand-link">
-                <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <a href="{{route('index')}}" class="brand-link">
+                <img src="../../dist/img/lwlogo.gif" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Loja 1.0</span>
             </a>
 
@@ -178,7 +179,7 @@
             <div class="float-right d-none d-sm-block">
 
             </div>
-            <strong>Software Management <a href="http://adminlte.io">West </a>.</strong> All rights
+            <strong>Software Management <a href="#">West </a>.</strong> All rights
             reserved.
         </footer>
 
