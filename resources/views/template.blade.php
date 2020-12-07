c4af90f0db866bbe34a014fae3682b3ce98407a6
Maou Yami <maou@yamitec.com>
2020-12-06 22:10:41 -0300

    update migrates

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -64,7 +64,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="{{route('index')}}" class="brand-link">
-                <img src="../../dist/img/lwlogo.gif" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="../../dist/img/lwlogo.gif" alt="Loja 1.0" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Loja 1.0</span>
             </a>
 
@@ -73,7 +73,7 @@
                 <!-- Sidebar user (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{Auth::user()->employee()->image ?? '../../dist/img/lwlogo.gif'}}" style="min-height:36px!important" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">{{ Auth::user()->employee()->name ?? Auth::user()->name }} </a>
@@ -159,6 +159,9 @@
             <div style="height: 100vh" class="d-none" id="spinner">
                 <div class="d-flex justify-content-center align-items-center  h-100">
                     <div class="text-center">
+                        <img src="../../dist/img/lwlogo.gif" alt="Loja 1.0" class="brand-image img-circle elevation-3" style="opacity: .8">
+                    </div>
+                    <div class="text-center">
                         <div class="spinner-border" style="width: 5rem; height: 5rem;" role="status">
                             <span class="sr-only">Carregando...</span>
                         </div>
@@ -193,7 +196,7 @@
 
     <!-- jQuery -->
     <script src="../../plugins/jquery/jquery.min.js"></script>
-    jQuery UI 1.11.4 -->
+    <!--jQuery UI 1.11.4 -->
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 
     <!-- INPUT MASK -->
@@ -374,6 +377,7 @@
                 });
             }
         });
+
         function moveCards() {
             // Make the dashboard widgets sortable Using jquery UI
             $('.connectedSortable').sortable({
