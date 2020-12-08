2d8ba053e1c6fa7f3a9ea41521fd514addd65170
Marcos Paulo <markus.rodrigues7@gmail.com>
2020-12-08 11:58:40 -0200

    css

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -23,6 +23,8 @@
     <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="../../css/style.css">
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed" style="font-size: 14px">
