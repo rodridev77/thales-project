407ef7583228e92b8c95cc7b8c89b4ee742eda81
Maou Yami <maou@yamitec.com>
2020-12-08 10:14:02 -0300

    create actions component

---
+++ resources/views/components/actions.blade.php
@@ -0,0 +1,3 @@
+<button class="btn btn-xs" onclick='loadViewInHome("{{route("$route.edit",$id)}}")' id="employee-edit"><img src="../../images/icon-edit.png" title="Editar" width="24px"></button>
+<button class="btn btn-xs" data-toggle="modal" data-target="#exampleModal" data-id="{{$id}}" id="employee-delete"><img src="../../images/icon-trash.png" title="Remover" width="24px"></button>
+<button class="btn btn-xs" onclick='loadViewInHome("{{route("$route.edit",$id)}}")' id="employee-view"><img src="../../images/icon-view.png" title="Ver" width="24px"></button>
